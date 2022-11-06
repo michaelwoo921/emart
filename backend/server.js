1909f31e16cf21743db0416b5290b4fc7d3131da
Jung Woo <michaelwoo921@gmail.com>
2022-11-06 01:20:06 -0700

    add env

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import path from 'path';
+import dotenv from 'dotenv';
+
 import express from 'express';
+
+dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`server running on port 5000`));
+app.listen(PORT, () => console.log(`server running on port 5000`));
